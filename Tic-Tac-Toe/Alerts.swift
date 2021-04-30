72c72861cc79fef06a772ec4c99f94eb23920e6c
John Kouris <jdkouris@gmail.com>
2021-04-30 08:27:35 -0700

    Implemented alerts for wins and draws and ability to reset the game

--- Tic-Tac-Toe/Alerts.swift
+++ Tic-Tac-Toe/Alerts.swift
@@ -5,4 +5,23 @@
 //  Created by John Kouris on 4/30/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    var title: Text
+    var message: Text
+    var buttonTitle: Text
+}
+
+struct AlertContext {
+    static let humanWin = AlertItem(title: Text("You win"),
+                                    message: Text("You are so smart. You beat the AI."),
+                                    buttonTitle: Text("Yeah!"))
+    static let computerWin = AlertItem(title: Text("Computer wins"),
+                                       message: Text("You are so dumb. You lost to the AI."),
+                                       buttonTitle: Text("Rematch"))
+    static let draw = AlertItem(title: Text("Draw"),
+                                message: Text("No one wins"),
+                                buttonTitle: Text("Try Again"))
+}
--- Tic-Tac-Toe/ContentView.swift
+++ Tic-Tac-Toe/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     
     @State private var moves: [Move?] = Array(repeating: nil, count: 9)
     @State private var isGameboardDisabled = false
+    @State private var alertItem: AlertItem?
     
     var body: some View {
         GeometryReader { geometry in
@@ -37,12 +38,33 @@ struct ContentView: View {
                         .onTapGesture {
                             if isSquareOccupied(in: moves, forIndex: i) { return }
                             moves[i] = Move(player: .human, boardIndex: i)
+                            
+                            if checkWinCondition(for: .human, in: moves) {
+                                alertItem = AlertContext.humanWin
+                                return
+                            }
+                            
+                            if checkForDraw(in: moves) {
+                                alertItem = AlertContext.draw
+                                return
+                            }
+                            
                             isGameboardDisabled = true
                             
                             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                                 let computerPosition = determineComputerMovePosition(in: moves)
                                 moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
                                 isGameboardDisabled = false
+                                
+                                if checkWinCondition(for: .computer, in: moves) {
+                                    alertItem = AlertContext.computerWin
+                                    return
+                                }
+                                
+                                if checkForDraw(in: moves) {
+                                    alertItem = AlertContext.draw
+                                    return
+                                }
                             }
                         }
                     }
@@ -51,6 +73,11 @@ struct ContentView: View {
             }
             .disabled(isGameboardDisabled)
             .padding()
+            .alert(item: $alertItem, content: { alertItem in
+                Alert(title: alertItem.title,
+                      message: alertItem.message,
+                      dismissButton: .default(alertItem.buttonTitle, action: resetGame))
+            })
         }
     }
     
@@ -66,6 +93,25 @@ struct ContentView: View {
         }
         
         return movePosition
+    }
+    
+    func checkWinCondition(for player: Player, in moves: [Move?]) -> Bool {
+        let winPatterns: Set<Set<Int>> = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+        
+        let playerMoves = moves.compactMap { $0 }.filter { $0.player == player }
+        let playerPositions = Set(playerMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns where pattern.isSubset(of: playerPositions) { return true }
+        
+        return false
+    }
+    
+    func checkForDraw(in moves: [Move?]) -> Bool {
+        return moves.compactMap { $0 }.count == 9
+    }
+    
+    func resetGame() {
+        moves = Array(repeating: nil, count: 9)
     }
 }
 
